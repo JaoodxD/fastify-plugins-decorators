9c6574ef3dcb353a9567953698dc662524f4413c
JaoodxD <jaood96@gmail.com>
2023-07-24 17:09:28 +0300

    add global util function with `decorate`

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 const fastify = require('fastify');
 
 const app = fastify();
+
+const superDuperFunction = () => ({ hello: 'world' });
+app.decorate('superDuperFunction', superDuperFunction);
+
 app.register(require('./plugin.js'));
 
 app.listen({ port: 3001 });
